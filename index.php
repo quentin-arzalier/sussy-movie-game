92139d56459a796294a63f87ca0d77a318b0a0e6
Quentin Arzalier <quentin.arzalier@free.fr>
2024-02-29 10:20:24 +0100

    Suite styles

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
+// Configuration
 require_once 'Config/dbconfig.php';
 require_once 'Config/apiconfig.php';
 
+// Controllers
+require_once "Controllers/HomeController.php";
 include_once "Controllers/MovieController.php";
 include_once "Controllers/ApiController.php";
+
+// Models
 include_once 'Models/Movie.php';
 
+// router
 require_once 'router.php';
