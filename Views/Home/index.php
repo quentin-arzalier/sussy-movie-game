16bb962c1f81fd9f66d4c25b6a54b5f9037b4fb3
quentin-arzalier <quentin.arzalier@free.fr>
2024-05-21 15:24:02 +0200

    Fix page jeu

--- Views/Home/index.php
+++ Views/Home/index.php
@@ -1,14 +1,12 @@
-<div>
-    <div class="custom-input">
-        <label for="movie-search-bar">
-            <i class="fa-solid fa-magnifying-glass"></i>
-        </label>
-        <input placeholder="Chercher un film" type="search" id="movie-search-bar">
-    </div>
-
-    <ul class="movie-list transparent game-list" id="movie-list-ajax">
-    </ul>
+<div class="custom-input">
+    <label for="movie-search-bar">
+        <i class="fa-solid fa-magnifying-glass"></i>
+    </label>
+    <input placeholder="Chercher un film" type="search" id="movie-search-bar">
 </div>
+
+<ul class="movie-list transparent" id="movie-list-ajax">
+</ul>
 
 <div id="game-attempts">
 
